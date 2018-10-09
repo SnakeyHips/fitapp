3e8ec328462ed765de835435c57e432ce351a5bf
Jake Gough <super.jake@hotmail.co.uk>
2018-10-09 15:32:13 +0100

    Update exercise.dart

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -6,15 +6,18 @@ class Exercise {
   int sets;
   int reps;
 
-  Exercise(this.name, this.description, this.areas, this.weight, this.sets, this.reps);
+  Exercise({this.name, this.description, this.areas, this.weight, this.sets, this.reps});
 
-  Exercise.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        description = json['description'],
-        areas = json['areas'],
-        weight = json['weight'],
-        sets = json['sets'],
-        reps = json['reps'];
+  factory Exercise.fromJson(Map<String, dynamic> json){
+    return new Exercise(
+      name: json['name'],
+        description: json['description'],
+        areas: json['areas'],
+        weight: json['weight'],
+        sets: json['sets'],
+        reps: json['reps']
+    );
+  }
 
   Map<String, dynamic> toJson() =>
       {'name': name, 'description': description, 'areas': areas, 'weight': weight, 'sets': sets, 'reps': reps};
