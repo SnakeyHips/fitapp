abf699721d71a0d2e165f90da50eafce39690136
Jake Gough <super.jake@hotmail.co.uk>
2018-10-09 15:32:50 +0100

    Update exercise_viewmodel.dart

--- lib/viewmodels/exercise_viewmodel.dart
+++ lib/viewmodels/exercise_viewmodel.dart
@@ -1,17 +1,54 @@
+import 'dart:convert';
 import 'dart:async' show Future;
+import 'dart:io';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:fitapp/models/exercise.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ExerciseViewModel {
  static Map<String, Exercise> exercises;
 
- static Future loadExercises() async {
-    exercises = {
-      "BP": new Exercise("Bench Press", "do it", "chest,shoulders,triceps", 0.0, 0 , 0),
-      "BS": new Exercise("Barbell Squat", "do it", "quadriceps,glutes,hamstrings", 0.0, 0 , 0),
-      "OHP": new Exercise("Overhead Press", "do it", "shoulders,chest", 0.0, 0 , 0),
-      "CD": new Exercise("Conventional Deadlift", "do it", "hamstrings,traps", 0.0, 0 , 0),
-      "BC": new Exercise("Bicep Curl", "do it", "biceps", 0.0, 0 , 0),
-      "DL":new Exercise("Dumbbell Lunges", "do it", "quadriceps,glutes,hamstrings,calves", 0.0, 0 , 0),
-    };
+  static Future<String> _loadAsset() async {
+    return await rootBundle.loadString('assets/exercises.json');
+  }
+
+  static Future<File> getFile() async{
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    return File('$path/exercises.json');
+  }
+
+  static Future load() async {
+    try {
+      File file = await getFile();
+      String exercisesFile = await file.readAsString();
+      if (exercisesFile != null) {
+        _loadJson(exercisesFile);
+      } else {
+        _loadJson(await _loadAsset());
+        await saveFile();
+      }
+    } catch (e) {
+      print(e);
+      _loadJson(await _loadAsset());
+      await saveFile();
+    }
+  }
+
+ static Future _loadJson(String routinesJson) async {
+    try {
+      exercises = new Map<String, Exercise>();
+      Map exercisesParsed = json.decode(routinesJson);
+      for(var key in exercisesParsed.keys){
+        exercises[key.toString()] = new Exercise.fromJson(exercisesParsed[key]);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+    static Future saveFile() async {
+    File file = await getFile();
+    file.writeAsString(json.encode(exercises));
   }
 }
