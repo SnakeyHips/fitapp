ab2cec5b3b54fa35e8c9e21e1718b1b2c6fe7f7f
Jake Gough <super.jake@hotmail.co.uk>
2018-10-17 16:20:34 +0100

    Update exercise_page.dart

--- lib/widgets/exercise_page.dart
+++ lib/widgets/exercise_page.dart
@@ -1,118 +1,103 @@
-import 'package:fitapp/models/exercise.dart';
+import 'package:fitapp/viewmodels/exercise_viewmodel.dart';
 import 'package:flutter/material.dart';
 
-class ExerciseDialog extends StatefulWidget {
-  final Exercise selectedExercise;
+class ExercisePage extends StatefulWidget {
+  final int selected;
 
-  ExerciseDialog({this.selectedExercise});
+  ExercisePage({this.selected});
 
   @override
-  State<StatefulWidget> createState() => ExerciseDialogState();
+  State createState() => ExercisePageState();
 }
 
-class ExerciseDialogState extends State<ExerciseDialog> {
-  final _formKey = GlobalKey<FormState>();
-
-  TextFormField weightField;
-  TextFormField setField;
-  TextFormField repField;
-  TextEditingController weightController = new TextEditingController();
-  TextEditingController setController = new TextEditingController();
-  TextEditingController repController = new TextEditingController();
-
-  @override
-  void initState() {
-    weightController.text = widget.selectedExercise.weight.toString();
-    setController.text = widget.selectedExercise.sets.toString();
-    repController.text = widget.selectedExercise.reps.toString();
-    weightField = new TextFormField(
-      controller: weightController,
-      decoration:
-          InputDecoration(labelText: "Weight", border: OutlineInputBorder()),
-      validator: (value) {
-        if (value.isEmpty || (double.tryParse(value) == null)) {
-          return "Please enter a valid decimal!";
-        }
-      },
-    );
-    setField = new TextFormField(
-      controller: setController,
-      decoration:
-          InputDecoration(labelText: "Sets", border: OutlineInputBorder()),
-      validator: (value) {
-        if (value.isEmpty || (int.tryParse(value) == null)) {
-          return "Please enter a valid number!";
-        }
-      },
-    );
-    repField = new TextFormField(
-      controller: repController,
-      decoration:
-          InputDecoration(labelText: "Reps", border: OutlineInputBorder()),
-      validator: (value) {
-        if (value.isEmpty || (int.tryParse(value) == null)) {
-          return "Please enter a valid number!";
-        }
-      },
-    );
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    weightController.dispose();
-    setController.dispose();
-    repController.dispose();
-    super.dispose();
-  }
-
+class ExercisePageState extends State<ExercisePage> {
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _formKey,
-        child: SimpleDialog(
-          title: Text(widget.selectedExercise.name),
-          contentPadding: EdgeInsets.all(12.0),
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(
-                    top: 8.0, bottom: 16.0, left: 12.0, right: 12.0),
-                child: weightField),
-            Padding(
-                padding: EdgeInsets.only(
-                    top: 8.0, bottom: 16.0, left: 12.0, right: 12.0),
-                child: setField),
-            Padding(
-                padding: EdgeInsets.only(
-                    top: 8.0, bottom: 8.0, left: 12.0, right: 12.0),
-                child: repField),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                FlatButton(
-                    child: Text("Cancel"),
-                    onPressed: () async {
-                      Navigator.of(context).pop();
-                    }),
-                FlatButton(
-                    child: Text(
-                      "Save",
-                      style: TextStyle(color: ThemeData.dark().accentColor),
-                    ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        widget.selectedExercise.weight =
-                            double.parse(weightController.text);
-                        widget.selectedExercise.sets =
-                            int.parse(setController.text);
-                        widget.selectedExercise.reps =
-                            int.parse(repController.text);
-                        Navigator.of(context).pop(widget.selectedExercise);
-                      }
-                    }),
-              ],
-            )
-          ],
-        ));
+    return Scaffold(
+            appBar: AppBar(
+                title: Text(ExerciseViewModel.exercises[widget.selected].name),
+            actions: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () async {
+                            // Routine temp = await showDialog(
+                            //     context: context,
+                            //     barrierDismissible: false,
+                            //     builder: (context) => RoutineDialog(
+                            //         selectedRoutine:
+                            //             RoutineViewModel.routines[widget.selected]));
+                            // setState(() {
+                            //   if (temp != null) {
+                            //     RoutineViewModel.routines[widget.selected] = temp;
+                            //   }
+                            // });
+                          }),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () async {
+                          await showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                               title: Text("Delete exercise"),
+                                content: Text("Are you sure you wish to delete the '" + ExerciseViewModel.exercises[widget.selected].name + "' exercise?"),
+                                actions: <Widget>[
+                                  FlatButton(
+                                  child: Text("Cancel"),
+                                  onPressed: () async {
+                                    Navigator.of(context).pop();
+                                  }),
+                                  FlatButton(
+                                  child: Text("Delete"),
+                                  onPressed: () async {
+                                    ExerciseViewModel.exercises.remove(ExerciseViewModel.exercises[widget.selected]);
+                                    await ExerciseViewModel.saveFile();
+                                    //also go through each routine to remove exercise
+                                    Navigator.of(context).pop();
+                                    Navigator.of(context).pop();
+                                  }),
+                                ],
+                            )
+                          );
+                        },
+                      )
+                    ]),
+            body: Material(
+                child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.only(bottom: 8.0),
+                                    child: Text(
+                                        ExerciseViewModel
+                                            .exercises[widget.selected].name,
+                                        style: TextStyle(
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.bold)))
+                              ]),
+                          Padding(
+                              padding: EdgeInsets.only(bottom: 8.0),
+                              child: Container(
+                                color: Colors.blue,
+                                height: 300.0,
+                              )),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.only(bottom: 8.0),
+                                    child: Text(ExerciseViewModel
+                                        .exercises[widget.selected]
+                                        .description))
+                              ])
+                        ],
+                      ),
+                    ))));
   }
 }
